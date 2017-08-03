cf6276bf73894e798b0976d49e85a268bcd6b640
José Roberto <joserobertolira@gmail.com>
2017-08-03 11:17:57 -0300

    Update and rename .js to index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './App.css'
+import './app.js'
+import Mod from './Mod.js'
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
